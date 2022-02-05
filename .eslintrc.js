0ea94fac8f771a229cb7cae032c2c285caddbfd2
Abner Rodrigues <abnerluis@infojr.com.br>
2022-02-05 12:10:39 -0300

    :rotating_light: lint: ignore js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
 	root: true,
 	extends: ['infojr-ts/next'],
+	ignorePatterns: ['**/*.js'],
 	// Option needed specially for monorepos where eslint doesn't know where to find tsconfig.json
 	parserOptions: {
 		project: 'tsconfig.json',
