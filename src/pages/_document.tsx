02ed254440ae95a58fb77a5dc93c903f77028d2f
Abner Rodrigues <abnerluis@infojr.com.br>
2022-02-06 18:36:04 -0300

    :fire: prune((pages/_document)): remove mistaken html tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,43 +41,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-
-          <meta
-            name="description"
-            content="Compre e anuncie imóveis, loteamentos, empreendimentos, terrenos, áreas e muito mais em toda a Bahia"
-          />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://kobraza.com.br/" />
-          <meta
-            property="og:title"
-            content="Kobraza Imóveis - Compre ou anuncie seu imóvel"
-          />
-          <meta
-            property="og:description"
-            content="Compre e anuncie imóveis, loteamentos, empreendimentos, terrenos, áreas e muito mais em toda a Bahia"
-          />
-          <meta property="og:image" content="" />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://kobraza.com.br" />
-          <meta
-            property="twitter:title"
-            content="Kobraza Imóveis - Compre ou anuncie seu imóvel"
-          />
-          <meta
-            property="twitter:description"
-            content="Compre e anuncie imóveis, loteamentos, empreendimentos, terrenos, áreas e muito mais em toda a Bahia"
-          />
-          <meta property="twitter:image" content="" />
-
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css"
-            integrity="sha512-wpPYUAdjBVSE4KJnH1VR1HeZfpl1ub8YT/NKx4PuQ5NmX2tKuGu6U/JRp5y+Y8XG2tV+wKQpNHVUX03MfMFn9Q=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
         </Head>
         <body>
           <Main />
