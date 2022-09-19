38743fdb74cfe266b65a63130f190c0452b3ae58
Edyo <edyoandrade@gmail.com>
2022-09-19 18:22:39 -0300

    :sparkles: feature(Cards da agenda): estilizacao mobile dos cards

--- src/components/Agenda/styles.ts
+++ src/components/Agenda/styles.ts
@@ -64,5 +64,109 @@ export const StyledAgenda = styled.section`
 `
 
 export const StyledAgendaCard = styled.div`
-    display: none;
+    display: flex;
+    background: solid white;
+    flex-direction: column;
+    border: solid 1px #2D2B784D;
+    width: 20.9375rem;
+
+    .container-img {
+        width: 100%;
+        position: relative;
+        height: 15.625rem;
+        display: flex;
+        align-items: flex-end;
+
+        p {
+            font-family: Merriweather;
+            background-color: #9A1A4B;
+            color: white;
+            width: 3.4375rem;
+            height: 4.625rem;
+            z-index: 1;
+            font-size: 1.5rem;
+            line-height: 1.25rem;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 0.9375rem;
+
+            span {
+                font-size: 0.875rem;
+                line-height: 1.125rem;
+            }
+        }
+    }
+
+    .container-baixo {
+        display: flex;
+        flex-direction: column;
+        padding: 1.5rem 1.75rem;
+
+        h4 {
+            width: 100%;
+            display: -webkit-box;
+            height: 3.875rem;
+            overflow: hidden;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            text-overflow: ellipsis;
+            font-family: Merriweather;
+            font-weight: 700;
+            font-size: 1.25rem;
+            color: #9A1A4B;
+            line-height: 1.875rem;
+            margin-bottom: 0.5rem;
+        }
+
+        .container-data {
+            display: flex;
+            gap: 0.5rem;
+            margin-bottom: 0.5rem;
+            align-items: center;
+
+            p {
+                font-size: 0.875rem;
+                line-height: 1.3125rem;
+                font-family: Arial;
+            }
+        }
+
+        .container-location {
+            display: flex;
+            margin-bottom: 0.5rem;
+            gap: 0.5rem;
+            align-items: center;
+
+            p {
+                font-size: 0.875rem;
+                line-height: 1.3125rem;
+                font-family: Arial;
+            }
+        }
+
+        .conteudo-agenda {
+            display: -webkit-box;
+            height: 3.875rem;
+            overflow: hidden;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+            text-overflow: ellipsis;
+            font-size: 0.875rem;
+            line-height: 1.3125rem;
+            margin-bottom: 1rem;
+            font-family: Arial;
+        }
+
+        a {
+            color: #2D2B78;
+            font-family: Merriweather;
+            font-size: 1rem;
+            font-weight: 700;
+            line-height: 2.0625rem;
+            width: fit-content;
+        }
+    }
 `
