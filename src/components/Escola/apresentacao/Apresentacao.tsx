99ccac90aa8cc243c6135f637e984d23fbb8e525
djairmaykon <djairmaykon@gmail.com>
2022-09-20 09:12:02 -0300

    :bug: fix(escola - apresentação): remove propriedade que o componente de paginaçao não recebe

--- src/components/Escola/apresentacao/Apresentacao.tsx
+++ src/components/Escola/apresentacao/Apresentacao.tsx
@@ -1,54 +1,60 @@
-import Image from 'next/image'; 
-import { useState } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 
-import Pagination from './Pagination';
-import { StyledPresentationContainer } from './styles';
+import Pagination from "./Pagination";
+import { StyledPresentationContainer } from "./styles";
 
 interface IApresentacaoItem {
-    conteudo: string,
-    imageAlt: string,
-    imageUrl: string,
-    titulo: string
+  conteudo: string;
+  imageAlt: string;
+  imageUrl: string;
+  titulo: string;
 }
 
 interface IApresentacao {
-    conteudos: IApresentacaoItem[]
+  conteudos: IApresentacaoItem[];
 }
 
 export default function Apresentacao({ conteudos }: IApresentacao) {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [contentPerPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [contentPerPage] = useState(1);
 
-    const lastContent = currentPage * contentPerPage; 
-    const firstContent = lastContent - contentPerPage; 
-    const currentContent = conteudos.slice(firstContent, lastContent); 
+  const lastContent = currentPage * contentPerPage;
+  const firstContent = lastContent - contentPerPage;
+  const currentContent = conteudos.slice(firstContent, lastContent);
 
-    const pages = [...Array(Math.ceil(conteudos.length / contentPerPage)).keys()];
+  const pages = [...Array(Math.ceil(conteudos.length / contentPerPage)).keys()];
 
-    return (
+  return (
     <StyledPresentationContainer>
-        {currentContent.map(item =>
-            <div className="presentation-content">
-                <div className="left-column" id='content'>
-                    <div className="image-container" id="image-container">
-                        <Image priority 
-                            alt={item.imageAlt} 
-                            className='image' 
-                            layout='fill'
-                            objectFit='cover' 
-                            src={item.imageUrl} 
-                        />                
-                    </div>
-                </div>
-                <div className="content-column-wrapper">
-                    <div className="content-column">
-                        <h1>Apresentação</h1>
-                        <h2>{item.titulo}</h2>
-                        <p>{item.conteudo}</p>
-                    </div>
-                    <Pagination isDarkVersion currentPage={currentPage} pages={pages} paginationFunction={setCurrentPage}/>
-                </div>
+      {currentContent.map((item) => (
+        <div className="presentation-content">
+          <div className="left-column" id="content">
+            <div className="image-container" id="image-container">
+              <Image
+                priority
+                alt={item.imageAlt}
+                className="image"
+                layout="fill"
+                objectFit="cover"
+                src={item.imageUrl}
+              />
             </div>
-        )}
-    </StyledPresentationContainer>);
+          </div>
+          <div className="content-column-wrapper">
+            <div className="content-column">
+              <h1>Apresentação</h1>
+              <h2>{item.titulo}</h2>
+              <p>{item.conteudo}</p>
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              pages={pages}
+              paginationFunction={setCurrentPage}
+            />
+          </div>
+        </div>
+      ))}
+    </StyledPresentationContainer>
+  );
 }
