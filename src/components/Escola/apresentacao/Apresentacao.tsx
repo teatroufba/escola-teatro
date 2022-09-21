609d5ec1b8c65628c2a57486d781551e812a9cbc
Maria Luísa <araujocostamalu@gmail.com>
2022-09-21 08:04:01 -0300

    :sparkles: feature: botão ver mais - seção apresentação

--- src/components/Escola/apresentacao/Apresentacao.tsx
+++ src/components/Escola/apresentacao/Apresentacao.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'; 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Pagination from './Pagination';
 import { StyledPresentationContainer } from './styles';
@@ -16,6 +16,10 @@ interface IApresentacao {
 }
 
 export default function Apresentacao({ conteudos }: IApresentacao) {
+    const [isShowingText, setIsShowingText] = useState(false) 
+    const [isClampedText, setIsClampedText] = useState(true)
+    const [isLongText, setIsLongText] = useState(false)
+    
     const [currentPage, setCurrentPage] = useState(1)
     const [contentPerPage] = useState(1)
 
@@ -25,8 +29,81 @@ export default function Apresentacao({ conteudos }: IApresentacao) {
 
     const pages = [...Array(Math.ceil(conteudos.length / contentPerPage)).keys()];
 
+    function showText() {
+        const text = document.getElementById('text-content'); 
+        const mainContainer = document.getElementById('main-container');
+        const button = document.getElementById('show-text-btn'); 
+        
+        if(text && mainContainer && button) {
+            
+            if(isShowingText) {
+                setIsShowingText(false);
+                setIsClampedText(true);
+                text.classList.add('hidden-text');
+                mainContainer.style.height = 
+                    window.innerWidth < 768 
+                    ? '100%' 
+                    : '800px';
+            }
+            else {
+                setIsShowingText(true); 
+                setIsClampedText(true);
+                text.classList.remove('hidden-text'); 
+                mainContainer.style.height = '100%';
+            }
+
+            if(window.innerWidth < 768) {
+                window.scrollTo({
+                    top: document.getElementById('title')?.offsetTop,
+                    behavior: 'smooth',
+                }); 
+            }
+
+        }
+    }
+
+    function hideText() {
+        if(isShowingText) {
+            setIsShowingText(false);
+            showText(); 
+        }
+    }
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const text = document.getElementById('text-content');
+            const button = document.getElementById('show-text-btn');   
+            
+            if(text && button) {
+
+                if(window.innerWidth > 320 && window.innerWidth <= 425) setIsLongText(text.innerHTML.length >= 250);
+                if(window.innerWidth > 425 && window.innerWidth <= 768) setIsLongText(text.innerHTML.length >= 350);
+                if(window.innerWidth > 768 && window.innerWidth <= 1200) setIsLongText(text.innerHTML.length >= 400);
+                if(window.innerWidth > 1200) setIsLongText(text.innerHTML.length >= 735);
+
+                
+                setIsClampedText(!(text.scrollHeight == text.clientHeight))
+
+                if(isLongText) {
+                    isClampedText 
+                    ? button.innerHTML = isShowingText ? 'Ver menos' : 'Ver mais'
+                    : button.innerHTML = 'Ver menos' 
+                } else {
+                    isClampedText ? null : button.innerHTML = ''
+                }
+            }
+        }
+    })
+
+    useEffect(() => {
+        if(isClampedText) {
+            const button = document.getElementById('show-text-btn');
+            if(button) button.innerHTML = 'Ver mais';
+        } 
+    }, [])
+
     return (
-    <StyledPresentationContainer>
+    <StyledPresentationContainer id='main-container'>
         {currentContent.map(item =>
             <div className="presentation-content">
                 <div className="left-column" id='content'>
@@ -42,11 +119,20 @@ export default function Apresentacao({ conteudos }: IApresentacao) {
                 </div>
                 <div className="content-column-wrapper">
                     <div className="content-column">
-                        <h1>Apresentação</h1>
+                        <h1 id='title'>Apresentação</h1>
                         <h2>{item.titulo}</h2>
-                        <p>{item.conteudo}</p>
+                        <div className="text-content">
+                            <p id='text-content' className='hidden-text'>{item.conteudo}</p>
+                            <button id='show-text-btn' onClick={() => showText()}>
+                                {''}
+                            </button>
+                        </div>
                     </div>
-                    <Pagination isDarkVersion currentPage={currentPage} pages={pages} paginationFunction={setCurrentPage}/>
+                    <Pagination 
+                        currentPage={currentPage} 
+                        pages={pages} 
+                        paginationFunction={setCurrentPage} 
+                        hideTextFunction={hideText}/>
                 </div>
             </div>
         )}
