331793d974c5b998817b960e7005ca7520d412e0
Maria Luísa <araujocostamalu@gmail.com>
2022-09-28 15:06:54 -0300

    :bug: fix(escola): corrigindo dependência do hook

--- src/components/Escola/corpo-docente/DocentesGrid.tsx
+++ src/components/Escola/corpo-docente/DocentesGrid.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import StaffCard from './DocenteCard';
-import Pagination from './Pagination';
-import { StyledStaff } from './styles';
+import StaffCard from "./DocenteCard";
+import Pagination from "./Pagination";
+import { StyledStaff } from "./styles";
 
 interface IDocente {
 	imagemAlt: string;
@@ -14,67 +14,87 @@ interface IDocente {
 	uid: string;
 }
 interface DocentesProps {
-    docentes: IDocente[],
-    largura: number,
-    tituloID: string
+	docentes: IDocente[];
+	largura: number;
+	tituloID: string;
 }
 
+export default function DocentesGrid({
+	docentes,
+	largura,
+	tituloID,
+}: DocentesProps) {
+	const [width, setWidth] = useState(0);
 
-export default function DocentesGrid ({ docentes, largura, tituloID }: DocentesProps) {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [cardsPerPage, setCardsPerPage] = useState(6);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [cardsPerPage, setCardsPerPage] = useState(6);
 
-    const lastCard = currentPage * cardsPerPage; 
-    const firstCard = lastCard - cardsPerPage; 
-    const currentCards = docentes.slice(firstCard, lastCard); 
+	const lastCard = currentPage * cardsPerPage;
+	const firstCard = lastCard - cardsPerPage;
+	const currentCards = docentes.slice(firstCard, lastCard);
 
-    const pages = [...Array(Math.ceil(docentes.length / cardsPerPage)).keys()];
+	const pages = [...Array(Math.ceil(docentes.length / cardsPerPage)).keys()];
 
-    useEffect(() => {
-        const element = document.getElementById('staff-container')
+	useEffect(() => {
+		function resize() {
+			setWidth(window.innerWidth);
+		}
 
-        if(largura > 1560) {
-            if(element!= null) {
-                element.style.gap = '24px'
-            }
-            setCardsPerPage(8);
-        };
+		window.addEventListener("resize", resize);
+		resize();
 
-        if(largura < 1560 && largura > 1190) {
-            if(element!= null) {
-                element.style.gap = '40px'
-            }
-            setCardsPerPage(6)
-        };
-        
-        if(largura < 1190) {  
-            if(element!= null) {
-                element.style.gap = '40px'
-            }
-            setCardsPerPage(4)
-        }
-    })
-    
-    return (
-    <div>
-        <StyledStaff id='staff-container'>
-            {currentCards.map(docente => (
-            <StaffCard key={docente.uid}
-                altImage={docente.imagemAlt} 
-                email={docente.email} 
-                imageUrl={docente.imagemUrl}
-                interests={docente.interesses} 
-                link={docente.link}
-                name={docente.nome}
-                uid={docente.uid}
-            /> )   
-            )}
-        </StyledStaff>
-        <Pagination 
-            currentPage = {currentPage} 
-            pages={pages} 
-            sectionTitleID = {tituloID}
-            paginationFunction={setCurrentPage} />    
-    </div>
-    )
+		return () => {
+			window.removeEventListener("resize", resize);
+		};
+	}, []);
+
+	useEffect(() => {
+		const element = document.getElementById("staff-container");
+
+		if (largura > 1560) {
+			if (element != null) {
+				element.style.gap = "24px";
+			}
+			setCardsPerPage(8);
+		}
+
+		if (largura < 1560 && largura > 1190) {
+			if (element != null) {
+				element.style.gap = "40px";
+			}
+			setCardsPerPage(6);
+		}
+
+		if (largura < 1190) {
+			if (element != null) {
+				element.style.gap = "40px";
+			}
+			setCardsPerPage(4);
+		}
+	}, [width]);
+
+	return (
+		<div>
+			<StyledStaff id="staff-container">
+				{currentCards.map((docente) => (
+					<StaffCard
+						key={docente.uid}
+						altImage={docente.imagemAlt}
+						email={docente.email}
+						imageUrl={docente.imagemUrl}
+						interests={docente.interesses}
+						link={docente.link}
+						name={docente.nome}
+						uid={docente.uid}
+					/>
+				))}
+			</StyledStaff>
+			<Pagination
+				currentPage={currentPage}
+				pages={pages}
+				sectionTitleID={tituloID}
+				paginationFunction={setCurrentPage}
+			/>
+		</div>
+	);
 }
