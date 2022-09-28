62d28f699427105fcdffa8a54b874d5285fee121
Maria Luísa <araujocostamalu@gmail.com>
2022-09-28 14:56:40 -0300

    :bug: fix: adicionando dependências ao hook

--- src/components/Escola/formas-de-ingresso/Ingresso.tsx
+++ src/components/Escola/formas-de-ingresso/Ingresso.tsx
@@ -1,109 +1,113 @@
+import ReadMore from "@/components/basics/read-more/ReadMore";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-import ReadMore from '@/components/basics/read-more/ReadMore';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import Pagination from "./Pagination";
+import { StyledEntryFormsContainer } from "./styles";
 
-import Pagination from './Pagination';
-import { StyledEntryFormsContainer } from './styles';
-  
-  interface IFormaIngresso {
-    link: string,
-    titulo: string
-  }
-  interface IConteudo {
-    conteudo: string,
-    titulo: string
-  }
-  interface FormasDeIngressoProps {
-    conteudos: IConteudo[],
-    formas: IFormaIngresso[],
-  }
+interface IFormaIngresso {
+	link: string;
+	titulo: string;
+}
+interface IConteudo {
+	conteudo: string;
+	titulo: string;
+}
+interface FormasDeIngressoProps {
+	conteudos: IConteudo[];
+	formas: IFormaIngresso[];
+}
 
-export default function FormasDeIngresso ({ conteudos, formas }: FormasDeIngressoProps) {
-    const [isShowingText, setIsShowingText] = useState(false) 
-    const [isClampedText, setIsClampedText] = useState(true)
-    const [isLongText, setIsLongText] = useState(false)
+export default function FormasDeIngresso({
+	conteudos,
+	formas,
+}: FormasDeIngressoProps) {
+	const [isShowingText, setIsShowingText] = useState(false);
+	const [isClampedText, setIsClampedText] = useState(true);
+	const [isLongText, setIsLongText] = useState(true);
 
-    const [currentPage, setCurrentPage] = useState(1)
-    const [contentPerPage] = useState(1)
+	const [currentPage, setCurrentPage] = useState(1);
+	const [contentPerPage] = useState(1);
 
-    const lastContent = currentPage * contentPerPage; 
-    const firstContent = lastContent - contentPerPage; 
-    const currentContent = conteudos.slice(firstContent, lastContent); 
+	const lastContent = currentPage * contentPerPage;
+	const firstContent = lastContent - contentPerPage;
+	const currentContent = conteudos.slice(firstContent, lastContent);
 
-    const pages = [...Array(Math.ceil(conteudos.length / contentPerPage)).keys()];
+	const pages = [...Array(Math.ceil(conteudos.length / contentPerPage)).keys()];
 
-    function hideText() {
-        if(isShowingText) {
-            const text = document.getElementById('entry-form-text-content'); 
-            const mainContainer = document.getElementById('entry-form-main-container');
+	function hideText() {
+		if (isShowingText) {
+			const text = document.getElementById("entry-form-text-content");
+			const mainContainer = document.getElementById(
+				"entry-form-main-container"
+			);
 
-            if(text && mainContainer) {
-                setIsShowingText(false);
-                text.classList.add('hidden-text');
-                mainContainer.style.height = 
-                    window.innerWidth < 768 
-                    ? '100%' 
-                    : '800px';
-                text.style.height = 
-                    window.innerWidth < 768 
-                    ? '100%' 
-                    : '300px';
+			if (text && mainContainer) {
+				setIsShowingText(false);
+				text.classList.add("hidden-text");
+				mainContainer.style.height = window.innerWidth < 768 ? "100%" : "800px";
+				text.style.height = window.innerWidth < 768 ? "100%" : "300px";
 
-                window.scrollTo({
-                    top: document.getElementById('entry-form-title')?.offsetTop,
-                    behavior: 'smooth',
-                }); 
-                    
-            }; 
-        }
-    }
+				window.scrollTo({
+					top: document.getElementById("entry-form-title")?.offsetTop,
+					behavior: "smooth",
+				});
+			}
+		}
+	}
 
-     return (
-        <StyledEntryFormsContainer id='entry-form-main-container'>
-            <h1 id='entry-form-title'>Formas de Ingresso</h1>
-            <div className="entry-content">
-                <div className="left-column">
-                    {formas.map((forma) => 
-                        <div className="entry-form">
-                            <Link passHref href={forma.link}>
-                                <a href={forma.link} rel='noopener noreferrer' target='_blank'><h3>{forma.titulo}</h3></a>
-                            </Link>
-                        </div>
-                    )}
-                </div>
+	return (
+		<StyledEntryFormsContainer id="entry-form-main-container">
+			<h1 id="entry-form-title">Formas de Ingresso</h1>
+			<div className="entry-content">
+				<div className="left-column">
+					{formas.map((forma) => (
+						<div className="entry-form">
+							<Link passHref href={forma.link}>
+								<a href={forma.link} rel="noopener noreferrer" target="_blank">
+									<h3>{forma.titulo}</h3>
+								</a>
+							</Link>
+						</div>
+					))}
+				</div>
 
-                <div className="right-column">
-                    {currentContent.map((forma) => 
-                        <div className="entry-form-info">
-                            <div className="entry-form-info-content">
-                                <h3>{ forma.titulo }</h3>
-                                <div className="text-content">
-                                    <p id='entry-form-text-content' className='hidden-text'>{ forma.conteudo }</p>
-                                    <ReadMore 
-                                        textID='entry-form-text-content'
-                                        mainContainerID='entry-form-main-container'
-                                        buttonID='entry-form-show-text-btn'
-                                        titleID='entry-form-title'
-                                        textHeight={300}
-                                        isShowingText={isShowingText}
-                                        isClampedText={isClampedText}
-                                        isLongText={isLongText}
-                                        setIsShowingText={setIsShowingText}
-                                        setIsClampedText={setIsClampedText}
-                                        setIsLongText={setIsLongText}
-                                    />
-                                </div>
-                            </div>
-                            <Pagination 
-                                currentPage={currentPage} 
-                                pages={pages} 
-                                paginationFunction={setCurrentPage}
-                                hideTextFunction={hideText} />
-                    </div>
-                    )}
-                </div>
-            </div>
-        </StyledEntryFormsContainer>
-    )
+				<div className="right-column">
+					{currentContent.map((forma) => (
+						<div className="entry-form-info">
+							<div className="entry-form-info-content">
+								<h3>{forma.titulo}</h3>
+								<div className="text-content">
+									<p id="entry-form-text-content" className="hidden-text">
+										{forma.conteudo}
+									</p>
+									<ReadMore
+										textID="entry-form-text-content"
+										mainContainerID="entry-form-main-container"
+										buttonID="entry-form-show-text-btn"
+										titleID="entry-form-title"
+										textHeight={300}
+										isShowingText={isShowingText}
+										isClampedText={isClampedText}
+										isLongText={isLongText}
+										currentPage={currentPage}
+										maxCharacters={815}
+										setIsShowingText={setIsShowingText}
+										setIsClampedText={setIsClampedText}
+										setIsLongText={setIsLongText}
+									/>
+								</div>
+							</div>
+							<Pagination
+								currentPage={currentPage}
+								pages={pages}
+								paginationFunction={setCurrentPage}
+								hideTextFunction={hideText}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</StyledEntryFormsContainer>
+	);
 }
--- src/components/basics/read-more/ReadMore.tsx
+++ src/components/basics/read-more/ReadMore.tsx
@@ -1,106 +1,122 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { StyledReadMoreBtn } from "./styles";
 
 type ReadMoreProps = {
-    textID: string,
-    mainContainerID: string,
-    buttonID: string,
-    titleID: string,
-    isShowingText: boolean,
-    isClampedText: boolean,
-    isLongText: boolean,
-    textHeight: number,
-    setIsShowingText: Dispatch<SetStateAction<boolean>>,
-    setIsClampedText: Dispatch<SetStateAction<boolean>>,
-    setIsLongText: Dispatch<SetStateAction<boolean>>
-}
+	textID: string;
+	mainContainerID: string;
+	buttonID: string;
+	titleID: string;
+	isShowingText: boolean;
+	isClampedText: boolean;
+	isLongText: boolean;
+	textHeight: number;
+	currentPage?: number;
+	maxCharacters: number;
+	setIsShowingText: Dispatch<SetStateAction<boolean>>;
+	setIsClampedText: Dispatch<SetStateAction<boolean>>;
+	setIsLongText: Dispatch<SetStateAction<boolean>>;
+};
 
-export default function ReadMore({ 
-    textID, 
-    mainContainerID, 
-    buttonID,
-    titleID,
-    textHeight,
-    isShowingText, 
-    isClampedText, 
-    isLongText, 
-    setIsShowingText, 
-    setIsClampedText, 
-    setIsLongText} 
-    : ReadMoreProps) {
+export default function ReadMore({
+	textID,
+	mainContainerID,
+	buttonID,
+	titleID,
+	textHeight,
+	isShowingText,
+	isClampedText,
+	isLongText,
+	currentPage,
+	maxCharacters,
+	setIsShowingText,
+	setIsClampedText,
+	setIsLongText,
+}: ReadMoreProps) {
+	const [showing, setShowing] = useState(false);
+	const [longText, setLongText] = useState(true);
 
-    function showText() {
-        const text = document.getElementById(textID); 
-        const mainContainer = document.getElementById(mainContainerID);
-        
-        if(text && mainContainer) {
-            
-            if(isShowingText) {
-                setIsShowingText(false);
-                setIsClampedText(true);
-                text.classList.add('hidden-text');
-                mainContainer.style.height = 
-                    window.innerWidth < 768 
-                    ? '100%' 
-                    : '800px';
-                text.style.height = 
-                    window.innerWidth < 768 
-                    ? '100%' 
-                    : `${textHeight}px`;
-            }
-            else {
-                setIsShowingText(true); 
-                setIsClampedText(false);
-                text.classList.remove('hidden-text'); 
-                mainContainer.style.height = '100%';
-                text.style.height = '90%';
-            }
+	function showText() {
+		const text = document.getElementById(textID);
+		const mainContainer = document.getElementById(mainContainerID);
 
-            window.scrollTo({
-                top: document.getElementById(titleID)?.offsetTop,
-                behavior: 'smooth',
-            }); 
-        }
-    }
+		if (text && mainContainer) {
+			if (isShowingText) {
+				setShowing(false);
+				setIsShowingText(false);
+				setIsClampedText(true);
+				text.classList.add("hidden-text");
+				mainContainer.style.height = window.innerWidth < 768 ? "100%" : "800px";
+				text.style.height =
+					window.innerWidth < 768 ? "100%" : `${textHeight}px`;
+			} else {
+				setShowing(true);
+				setIsShowingText(true);
+				setIsClampedText(false);
+				text.classList.remove("hidden-text");
+				mainContainer.style.height = "100%";
+				text.style.height = "90%";
+			}
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            const text = document.getElementById(textID);
-            const button = document.getElementById(buttonID);   
-            
-            if(text && button) {
+			window.scrollTo({
+				top: document.getElementById(titleID)?.offsetTop,
+				behavior: "smooth",
+			});
+		}
+	}
 
-                if(window.innerWidth > 320 && window.innerWidth <= 425) setIsLongText(text.innerHTML.length >= 250);
-                if(window.innerWidth > 425 && window.innerWidth <= 768) setIsLongText(text.innerHTML.length >= 350);
-                if(window.innerWidth > 768 && window.innerWidth <= 1200) setIsLongText(text.innerHTML.length >= 400);
-                if(window.innerWidth > 1200) setIsLongText(text.innerHTML.length >= 494);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const text = document.getElementById(textID);
+			const button = document.getElementById(buttonID);
 
-                
-                setIsClampedText(!(text.scrollHeight == text.clientHeight))
+			if (text && button) {
+				if (window.innerWidth > 320 && window.innerWidth <= 425)
+					setIsLongText(text.innerHTML.length >= 250);
+				if (window.innerWidth > 425 && window.innerWidth <= 768)
+					setIsLongText(text.innerHTML.length >= 300);
+				if (window.innerWidth > 768 && window.innerWidth <= 1200)
+					setIsLongText(text.innerHTML.length >= 400);
+				if (window.innerWidth > 1200)
+					setIsLongText(text.innerHTML.length >= maxCharacters);
 
-                if(isLongText) {
-                    isClampedText 
-                    ? button.innerHTML = isShowingText ? 'Ver menos' : 'Ver mais'
-                    : button.innerHTML = isShowingText ? 'Ver menos' : '' 
-                } else {
-                    button.innerHTML = ''
-                }
-            }
-        }
-    })
+				setIsClampedText(!(text.scrollHeight == text.clientHeight));
 
-    useEffect(() => {
-        if(isClampedText && isLongText) {
-            const button = document.getElementById(buttonID);
-            if(button) button.innerHTML = 'Ver mais';
-        } 
-    }, [])
+				if (longText || isLongText) {
+					button.innerHTML =
+						isClampedText && !isShowingText ? "Ver mais" : "Ver menos";
+				} else {
+					button.innerHTML = "";
+				}
+			}
+		}
+	}, [showing, isClampedText, currentPage]);
 
-    return (
-        <StyledReadMoreBtn>
-            <button id={buttonID} onClick={() => showText()}>
-                {''}
-            </button>
-        </StyledReadMoreBtn>
-    )
+	useEffect(() => {
+		const text = document.getElementById(textID);
+		if (text) {
+			if (window.innerWidth > 320 && window.innerWidth <= 425)
+				setLongText(text.innerHTML.length >= 250);
+			if (window.innerWidth > 425 && window.innerWidth <= 768)
+				setLongText(text.innerHTML.length >= 300);
+			if (window.innerWidth > 768 && window.innerWidth <= 1200)
+				setLongText(text.innerHTML.length >= 400);
+			if (window.innerWidth > 1200)
+				setLongText(text.innerHTML.length >= maxCharacters);
+		}
+	}, [currentPage]);
+
+	useEffect(() => {
+		if (isClampedText && isLongText) {
+			const button = document.getElementById(buttonID);
+			if (button) button.innerHTML = "Ver mais";
+		}
+	}, []);
+
+	return (
+		<StyledReadMoreBtn>
+			<button id={buttonID} onClick={() => showText()}>
+				{""}
+			</button>
+		</StyledReadMoreBtn>
+	);
 }
