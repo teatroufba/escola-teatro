0a28cd4eede387ed9401cee87958f871ad569beb
Lucas Azevedo <lucasazevedooliveira@outlook.com.br>
2022-09-30 16:38:13 -0300

    :bug: fix(banner home): responsividade
    ISSUES CLOSED: 25

--- src/components/Home/banner/index.tsx
+++ src/components/Home/banner/index.tsx
@@ -6,7 +6,7 @@ import { LineIcon } from './icons'
 
 const BannerStyled = styled.div`
   display: block;
-  background-color: #282b62;
+  height: calc(100% - 1000px);
 
   .carousel {
     max-width: 1920px;
@@ -23,17 +23,75 @@ const BannerStyled = styled.div`
   }
 
   .banner-control {
-    position: absolute;
+    position: relative;
     display: flex;
-    width: 64.375vw;
-    justify-content: right;
+    justify-content: space-between;
+    align-items: end;
+    width: 75vw;
     left: 0;
     right: 0;
-    top: min(calc(100vw), 798px);
+    top: -235px;
     bottom: 0;
     margin: auto;
+    gap: 10px;
 
-    @media screen and (max-width: 768px) {
+    .content {
+      color: white;
+      display: flex;
+      flex-direction: column;
+      gap: 25px;
+
+      h1 {
+        font-family: 'Merriweather';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 3rem;
+      }
+
+      p {
+        font-family: 'Arial';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 1rem;
+        max-width: 500px;
+      }
+
+      .content-button {
+        display: flex;
+
+        a {
+          background-color: #9a1a4b;
+          width: 40px;
+          height: 40px;
+          border: none;
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+        }
+
+        small {
+          background-color: #282b62;
+          display: flex;
+          justify-content: center;
+          padding: 10px 20px;
+          font-family: 'Merriweather';
+          font-style: normal;
+          font-weight: 700;
+          font-size: 0.875rem;
+        }
+      }
+    }
+
+    @media screen and (max-width: 1300px) {
+      top: -90px;
+      justify-content: right;
+
+      .content {
+        display: none;
+      }
+    }
+
+    @media screen and (max-width: 1300px) {
       position: static;
       background-color: #282b62;
       width: 100vw;
@@ -42,21 +100,24 @@ const BannerStyled = styled.div`
     }
 
     .pagination {
-      background-color: #282b62;
       display: flex;
-      justify-content: center;
-      width: 484px;
-      height: 40px;
-      color: white;
-      padding: 0 15px;
-      align-items: center;
-      gap: 10px;
 
-      
+      .pagination-control {
+        background-color: #282b62;
+        display: flex;
+        justify-content: center;
+        width: 484px;
+        height: 40px;
+        color: white;
+        padding: 0 15px;
+        align-items: center;
+        gap: 10px;
 
-      p {
-        font-family: 'Merriweather';
-        font-weight: 700;
+
+        p {
+          font-family: 'Merriweather';
+          font-weight: 700;
+        }
       }
     }
 
@@ -68,13 +129,21 @@ const BannerStyled = styled.div`
       display: block;
     }
 
-    @media screen and (max-width: 768px) {
+    @media screen and (max-width: 1000px) {
       .mobile {
         display: block;
       }
 
       .desktop {
         display: none;
+      }
+
+      .pagination {
+        display: flex;
+
+        .pagination-control {
+          width: calc(100vw - 40px);
+        }
       }
     }
 
@@ -146,7 +215,7 @@ function Banner() {
       }
       setTimeout(() => {
         setScrolling(false)
-      }, carousel.current.clientWidth -160);
+      }, carousel.current.clientWidth*0.5);
     }
   }
 
@@ -166,7 +235,7 @@ function Banner() {
       }
       setTimeout(() => {
         setScrolling(false)
-      }, carousel.current.clientWidth -100);
+      }, carousel.current.clientWidth + 400);
     }
   }
 
@@ -188,42 +257,57 @@ function Banner() {
         <img alt="" src="/banner-1.jpg" />
       </div>
       <div className="banner-control">
-        <div className="pagination">
-
-        
-          <p>01</p>
-          {carouselPagination.map((page: boolean, index: number) => (
-            <div key={Number(index)} className={index > 6 ? 'desktop' : ''}>
-              <LineIcon hover={page} size={20} />
-            </div>
-          ))}
-          <p className="desktop">13</p>
-          <p className="mobile">07</p>
+        <div className='content'>
+          <h1>Lorem Ipsum</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie elit vitae odio blandit mauris ac aenean pellentesque.</p>
+          <div className='content-button'>
+            <a className='desktop'>
+              <Image
+                alt="ícone de seta para esquerda"
+                height={14}
+                src="/arrow-right.svg"
+                width={7}
+              />
+            </a>
+            <small>Ver mais</small>
+          </div>
         </div>
-        <button
-          className={
-            carouselScrollLeft >= carouselTotalScroll
-              ? 'desktop desactive'
-              : 'desktop'
-          }
-          onClick={scroll}
-          disabled={scrolling}
-        >
-          <Image
-            alt="ícone de seta para esquerda"
-            height={14}
-            src="/arrow-right.svg"
-            width={7}
-          />
-        </button>
-        <button className="mobile" onClick={scrollMobile} disabled={scrolling}>
-          <Image
-            alt="ícone de seta para esquerda"
-            height={14}
-            src="/arrow-right.svg"
-            width={7}
-          />
-        </button>
+        <div className="pagination">
+          <div className="pagination-control">
+            <p>01</p>
+            {carouselPagination.map((page: boolean, index: number) => (
+              <div key={Number(index)} className={index > 6 ? 'desktop' : ''}>
+                <LineIcon hover={page} size={20} />
+              </div>
+            ))}
+            <p className="desktop">13</p>
+            <p className="mobile">07</p>
+          </div>
+          <button
+            className={
+              carouselScrollLeft >= carouselTotalScroll
+                ? 'desktop desactive'
+                : 'desktop'
+            }
+            onClick={scroll}
+            disabled={scrolling}
+          >
+            <Image
+              alt="ícone de seta para esquerda"
+              height={14}
+              src="/arrow-right.svg"
+              width={7}
+            />
+          </button>
+          <button className="mobile" onClick={scrollMobile} disabled={scrolling}>
+            <Image
+              alt="ícone de seta para esquerda"
+              height={14}
+              src="/arrow-right.svg"
+              width={7}
+            />
+          </button>
+        </div>
       </div>
     </BannerStyled>
   )
