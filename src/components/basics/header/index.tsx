436cfa63358c4bbea61f33223ae1bc03a6107e88
Maria Luísa <araujocostamalu@gmail.com>
2022-10-10 10:19:44 -0300

    :sparkles: feature: submenu do header

--- src/components/basics/header/index.tsx
+++ src/components/basics/header/index.tsx
@@ -1,111 +1,235 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useState } from "react";
 
-import image from '@/public/brasao.png'
+import image from "@/public/brasao.png";
 
-import { HeaderContainer, HeaderInferior, HeaderSuperior } from './styles'
+import {
+	HeaderContainer,
+	HeaderInferior,
+	HeaderSuperior,
+	Submenu,
+} from "./styles";
+
+interface SubmenuOption {
+	id: string;
+	option: string;
+}
+
+interface SubmenuOptions {
+	options: SubmenuOption[];
+}
 
 function Header() {
-  return (
-    <HeaderContainer>
-      <HeaderSuperior>
-        <div className="header">
-          <div className='logo-img'>
-            <Image 
-              alt="brasao" 
-              src={image} 
-             
-            />
-          </div>
-           
-          <p>Escola de Teatro da UFBA</p>
-        </div>
+	const [options, setOptions] = useState<SubmenuOption[]>([]);
+	const [section, setSection] = useState<string>("");
+	const [isShowing, setIsShowing] = useState(false);
 
-        <div className='social-navbar'>
+	function showSubmenu(section: string) {
+		switch (section) {
+			case "escola":
+				if (typeof document != "undefined") {
+					document.getElementById("escola")?.classList.add("hover-state");
+				}
+				setIsShowing(true);
+				setSection("escola");
+				setOptions([
+					{ id: "", option: "Apresentação" },
+					{ id: "", option: "Ingresso" },
+					{ id: "", option: "Setores e Instâncias" },
+					{ id: "", option: "Corpo Docente" },
+					{ id: "", option: "Corpo Técnico" },
+					{ id: "", option: "Contato" },
+				]);
+				break;
 
-          <div  
-              id="ham-btn"
-          >
-            <div className="ham-btn-bar" />
-            <div className="ham-btn-bar" />
-            <div className="ham-btn-bar" />
-          </div>
+			case "ensino":
+				if (typeof document != "undefined") {
+					document.getElementById("ensino")?.classList.add("hover-state");
+				}
+				setIsShowing(true);
+				setSection("ensino");
+				setOptions([
+					{ id: "", option: "Graduação" },
+					{ id: "", option: "Pós Graduação" },
+					{ id: "", option: "Grupos de Pesquisa" },
+					{ id: "", option: "Produção Acadêmica" },
+				]);
+				break;
 
+			case "extensao":
+				if (typeof document != "undefined") {
+					document.getElementById("extensao")?.classList.add("hover-state");
+				}
+				setIsShowing(true);
+				setSection("extensao");
+				setOptions([
+					{ id: "", option: "Cia de Teatro" },
+					{ id: "", option: "Curso Livre" },
+					{ id: "", option: "Ato de 4" },
+					{ id: "", option: "Outros Projetos" },
+				]);
+				break;
 
+			case "espacos":
+				if (typeof document != "undefined") {
+					document.getElementById("espacos")?.classList.add("hover-state");
+				}
+				setIsShowing(true);
+				setSection("espacos");
+				setOptions([
+					{ id: "", option: "Teatro Martin Gonçalves" },
+					{ id: "", option: "Pavilhão de Aulas" },
+					{ id: "", option: "Casarão" },
+					{ id: "", option: "Sala 05" },
+					{ id: "", option: "Galeria Nilda Spencer" },
+					{ id: "", option: "Carpintaria" },
+					{ id: "", option: "Rouparia" },
+					{ id: "", option: "Biblioteca" },
+				]);
+				break;
+		}
+	}
 
-          <ul className="social">
-            <a href="http://www.facebook.com.br">
-              <input
-                alt="facebook"
-                className="facebook"
-                height={50}
-                src="./icon-facebook.svg"
-                type="image"
-                width={50}
-              />
-            </a>
-            <a href="http://www.instagram.com.br">
-              <input
-                alt="instagram"
-                className="instagram"
-                height={50}
-                src="./icon-instagram.svg"
-                type="image"
-                width={50}
-              />
-            </a>
-            <a href="http://www.youtube.com.br">
-              <input
-                alt="youtube"
-                className="youtube"
-                height={50}
-                src="./icon-youtube.svg"
-                type="image"
-                width={50}
-              />
-            </a>
-            <a href="http://www.flickr.com.br">
-              <input
-                alt="flickr"
-                className="flickr"
-                height={50}
-                src="./icon-flickr.svg"
-                type="image"
-                width={50}
-              />
-            </a>
-          </ul>
-        </div>
-      </HeaderSuperior>
+	function hideSubmenu(section: string) {
+		if (typeof document != "undefined") {
+			document.getElementById(section)?.classList.remove("hover-state");
+		}
+		setIsShowing(false);
+	}
 
-      <HeaderInferior>
-           <ul className="menu">
-              <li>
-                <a href="/">Início</a>
-              </li>
-              <li>
-                <a href="/escola">A Escola</a>
-              </li>
-              <li>
-                <a href="/ensino">Ensino e Pesquisa</a>
-              </li>
-              <li>
-                <a href="/extensao">Extensão</a>
-              </li>
-              <li>
-                <a href="/espacos">Espaços</a>
-              </li>
-              <li>
-                <a href="/agenda">Agenda</a>
-              </li>
-              <li>
-                <a href="/galeria">Galeria Virtual</a>
-              </li>
-          </ul> 
+	return (
+		<HeaderContainer>
+			<HeaderSuperior>
+				<div className="header">
+					<div className="logo-img">
+						<Image alt="brasao" src={image} />
+					</div>
 
-         
-      </HeaderInferior>
-    </HeaderContainer>
-  )
+					<p>Escola de Teatro da UFBA</p>
+				</div>
+
+				<div className="social-navbar">
+					<div id="ham-btn">
+						<div className="ham-btn-bar" />
+						<div className="ham-btn-bar" />
+						<div className="ham-btn-bar" />
+					</div>
+
+					<ul className="social">
+						<a href="http://www.facebook.com.br">
+							<input
+								alt="facebook"
+								className="facebook"
+								height={50}
+								src="./icon-facebook.svg"
+								type="image"
+								width={50}
+							/>
+						</a>
+						<a href="http://www.instagram.com.br">
+							<input
+								alt="instagram"
+								className="instagram"
+								height={50}
+								src="./icon-instagram.svg"
+								type="image"
+								width={50}
+							/>
+						</a>
+						<a href="http://www.youtube.com.br">
+							<input
+								alt="youtube"
+								className="youtube"
+								height={50}
+								src="./icon-youtube.svg"
+								type="image"
+								width={50}
+							/>
+						</a>
+						<a href="http://www.flickr.com.br">
+							<input
+								alt="flickr"
+								className="flickr"
+								height={50}
+								src="./icon-flickr.svg"
+								type="image"
+								width={50}
+							/>
+						</a>
+					</ul>
+				</div>
+			</HeaderSuperior>
+
+			<HeaderInferior>
+				<ul className="menu">
+					<li>
+						<a href="/">Início</a>
+					</li>
+					<li>
+						<a
+							id="escola"
+							href="/escola"
+							onMouseEnter={() => showSubmenu("escola")}
+							onMouseLeave={() => hideSubmenu("escola")}
+						>
+							A Escola
+						</a>
+					</li>
+					<li>
+						<a
+							id="ensino"
+							href="/ensino"
+							onMouseEnter={() => showSubmenu("ensino")}
+							onMouseLeave={() => hideSubmenu("ensino")}
+						>
+							Ensino e Pesquisa
+						</a>
+					</li>
+					<li>
+						<a
+							id="extensao"
+							href="/extensao"
+							onMouseEnter={() => showSubmenu("extensao")}
+							onMouseLeave={() => hideSubmenu("extensao")}
+						>
+							Extensão
+						</a>
+					</li>
+					<li>
+						<a
+							id="espacos"
+							href="/espacos"
+							onMouseEnter={() => showSubmenu("espacos")}
+							onMouseLeave={() => hideSubmenu("espacos")}
+						>
+							Espaços
+						</a>
+					</li>
+					<li>
+						<a href="/agenda">Agenda</a>
+					</li>
+					<li>
+						<a href="/galeria">Galeria Virtual</a>
+					</li>
+				</ul>
+			</HeaderInferior>
+
+			{isShowing ? (
+				<Submenu>
+					<ul
+						className="submenu"
+						onMouseEnter={() => showSubmenu(section)}
+						onMouseLeave={() => hideSubmenu(section)}
+					>
+						{options.map((item) => (
+							<li>
+								<a href="/">{item.option}</a>
+							</li>
+						))}
+					</ul>
+				</Submenu>
+			) : null}
+		</HeaderContainer>
+	);
 }
-export default Header
+export default Header;
--- src/components/basics/header/styles.tsx
+++ src/components/basics/header/styles.tsx
@@ -1,190 +1,205 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 // TODO - Ajustar responsividade
 
-
-
 export const HeaderContainer = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  background-color: #fff;
-  gap: 35px;
-`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	width: 100%;
+	background-color: #fff;
+	gap: 35px;
+`;
 export const HeaderSuperior = styled.div`
-  
+	background-color: #fff;
 
-  background-color: #fff;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	padding-top: 3%;
+	height: 100%;
+	width: 100%;
 
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding-top: 3%;
-  height: 100%;
-  width: 100%;
+	@media (max-width: 1200px) {
+		padding: 35px 64px;
+		padding: 0;
+		justify-content: start;
+		flex-direction: column-reverse;
+		gap: 25px;
+	}
 
-  @media (max-width: 1200px) {
-    padding: 35px 64px;
-    padding: 0;
-    justify-content: start;
-    flex-direction: column-reverse;
-    gap: 25px;
-  }
+	.social-navbar {
+		@media (max-width: 1200px) {
+			background-color: #282b62;
+			align-items: center;
+			justify-content: space-between;
+			display: flex;
 
-  .social-navbar {
+			margin: 0px;
+			padding: 18px 25px;
 
-    @media (max-width: 1200px) {
-    background-color: #282b62;
-    align-items: center;
-    justify-content: space-between;
-    display: flex;
-  
-    margin: 0px;
-    padding: 18px 25px;
+			width: 100%;
+		}
+	}
 
-    
-    width: 100%;
-    }
-    
-  }
+	.social {
+		gap: 25px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 
-  .social {
-    gap: 25px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+		@media (max-width: 1200px) {
+			a input {
+				filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(345deg)
+					brightness(101%) contrast(102%);
+			}
+		}
 
- 
+		@media (max-width: 600px) {
+			a input {
+				width: 20px;
+				height: 20px;
+			}
+		}
+	}
 
-    @media (max-width: 1200px) {
-      a input {
-        filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(345deg) brightness(101%) contrast(102%);
-        
-      }
-    }
+	#ham-btn {
+		display: flex;
+		flex-direction: column;
+		gap: 5px;
 
-    @media (max-width: 600px) {
-      a input {
-        width: 20px;
-        height: 20px;
-      }
-      
-    }
+		display: none;
+		.ham-btn-bar {
+			width: 30px;
+			height: 3px;
+			background-color: #fff;
+			transition: 0.4s;
+		}
 
-    
-  }
+		@media (max-width: 1200px) {
+			display: flex;
+		}
+	}
 
-  #ham-btn {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
+	.header {
+		gap: 34px;
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
 
-    display: none;
-    .ham-btn-bar {
-      width: 30px;
-      height: 3px;
-      background-color: #fff;
-      transition: 0.4s;
-    }
+		@media (max-width: 1200px) {
+			justify-content: start;
+			align-items: center;
+			gap: 16px;
 
-    @media (max-width: 1200px) {
-      display: flex;
-    }
-  }
+			p {
+				max-width: 152px;
+				max-height: 44px;
+				font-size: 18px;
+				font-weight: 400;
+				font-size: 18px;
+				line-height: 120%;
+			}
 
+			.img-logo {
+				height: 80px;
+				width: 71px;
+			}
+		}
+	}
 
-  .header {
-    gap: 34px;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-
-
-    @media (max-width: 1200px) {
-      justify-content: start;
-      align-items: center;
-      gap: 16px;
-    
-      p {
-        max-width: 152px;
-        max-height: 44px;
-        font-size: 18px;
-        font-weight: 400;
-        font-size: 18px;
-        line-height: 120%;
-
-        
-      }
-
-      .img-logo {
-        height: 80px;
-        width: 71px;
-      }
-      
-    }
-  }
-
-  p {
-    color: #282b62;
-    font-size: 36px;
-    line-height: 48px;
-    font-family: 'Arial';
-  }
-`
+	p {
+		color: #282b62;
+		font-size: 36px;
+		line-height: 48px;
+		font-family: "Arial";
+	}
+`;
 
 export const HeaderInferior = styled.nav`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0px;
-  margin: 0px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 0px;
+	margin: 0px;
 
-  
+	@media (max-width: 1200px) {
+		display: none;
+	}
+	.menu {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 24px;
+		line-height: 30px;
+		font-family: "Merriweather";
+		background-color: #2d2b78;
+		padding: 0;
+		list-style: none;
+		padding: 0px;
+		margin: 0px;
+		width: 100%;
+		@media (max-width: 640px) {
+		}
+	}
+	.btn-mobile {
+		display: none;
+	}
+	.menu li {
+		display: inline;
+	}
 
-  @media (max-width: 1200px) {
-    display: none;
-    
-  }
-  .menu {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 24px;
-    line-height: 30px;
-    font-family: 'Merriweather';
-    background-color: #2d2b78;
-    padding: 0;
-    list-style: none;
-    padding: 0px;
-  margin: 0px;
-    width: 100%;
-    @media (max-width: 640px) {
-    }
-  }
-  .btn-mobile {
-    display: none;
-  }
-  .menu li {
-    display: inline;
-  }
+	.menu li a {
+		color: #ffffff;
+		padding: 20px;
+		display: inline-block;
+		text-decoration: none;
+		transition-duration: 0.3s;
+	}
 
-  .menu li a {
-    color: #ffffff;
-    padding: 20px;
-    display: inline-block;
-    text-decoration: none;
-    transition-duration: 0.3s;
-  }
+	.menu li a:hover {
+		background-color: #9a1a4b;
+	}
 
-  .menu li a:hover {
-    background-color: #9a1a4b;
-  }
-`
+	.hover-state {
+		background-color: #9a1a4b;
+	}
+`;
 
+export const Submenu = styled.div`
+	margin-top: -35px;
+	width: 100%;
+
+	.submenu {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 24px;
+		line-height: 30px;
+		font-family: "Merriweather";
+		background-color: #2d2b78;
+		padding: 0;
+		list-style: none;
+		padding: 0px;
+		margin: 0px;
+		width: 100%;
+	}
+
+	.submenu li a {
+		color: #ffffff;
+		padding: 20px;
+		display: inline-block;
+		text-decoration: none;
+		transition-duration: 0.3s;
+	}
+
+	.submenu li a:hover {
+		color: #9a1a4b;
+	}
+`;
 
 export const MobileNavbar = styled.nav`
-  display : flex;
-`
+	display: flex;
+`;
