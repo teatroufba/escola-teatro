e15bbc87edd100a5d7c1c9bf2e50a20565c427d6
Maria Luísa <araujocostamalu@gmail.com>
2022-10-10 11:07:54 -0300

    :art: style: ajustando as classes do submenu

--- src/components/basics/header/styles.tsx
+++ src/components/basics/header/styles.tsx
@@ -168,38 +168,6 @@ export const HeaderInferior = styled.nav`
 	}
 `;
 
-export const Submenu = styled.div`
-	margin-top: -35px;
-	width: 100%;
-
-	.submenu {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 24px;
-		line-height: 30px;
-		font-family: "Merriweather";
-		background-color: #2d2b78;
-		padding: 0;
-		list-style: none;
-		padding: 0px;
-		margin: 0px;
-		width: 100%;
-	}
-
-	.submenu li a {
-		color: #ffffff;
-		padding: 20px;
-		display: inline-block;
-		text-decoration: none;
-		transition-duration: 0.3s;
-	}
-
-	.submenu li a:hover {
-		color: #9a1a4b;
-	}
-`;
-
 export const MobileNavbar = styled.nav`
 	display: flex;
 `;
