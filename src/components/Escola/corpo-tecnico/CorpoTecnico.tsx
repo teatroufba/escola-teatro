f0c312739498dbd0225c9a395ad1abf2a5744429
Maria Luísa <araujocostamalu@gmail.com>
2022-10-17 07:29:47 -0300

    :sparkles: feature(escola): responsivo corpo tecnico

--- src/components/Escola/corpo-tecnico/CorpoTecnico.tsx
+++ src/components/Escola/corpo-tecnico/CorpoTecnico.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import CardCorpoTecnico from "./CardCorpoTecnico";
 import { StyledStaffTable, StyledStaffTableContainer } from "./styles";
 
 interface ICorpoTecnico {
@@ -11,29 +13,46 @@ interface ICorpoTecnicoArray {
 }
 
 export default function CorpoTecnico({ corpoTecnico }: ICorpoTecnicoArray) {
+	const [width, setWidth] = useState(0);
+
+	useEffect(() => {
+		function handleResize() {
+			setWidth(window.innerWidth);
+		}
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
 	return (
 		<StyledStaffTableContainer>
 			<h1 id="corpo-tecnico">Corpo Técnico</h1>
 			<div className="table-container">
-				<StyledStaffTable>
-					<thead>
-						<tr>
-							<th>Nome</th>
-							<th>Função</th>
-							<th>E-mail</th>
-						</tr>
-					</thead>
-
-					<tbody>
-						{corpoTecnico.map((item, index) => (
-							<tr key={`membro${index}`}>
-								<td>{item.nome}</td>
-								<td>{item.funcao}</td>
-								<td>{item.email}</td>
+				{width > 775 ? (
+					<StyledStaffTable>
+						<thead>
+							<tr>
+								<th>Nome</th>
+								<th>Função</th>
+								<th>E-mail</th>
 							</tr>
-						))}
-					</tbody>
-				</StyledStaffTable>
+						</thead>
+
+						<tbody>
+							{corpoTecnico.map((item, index) => (
+								<tr key={`membro${index}`}>
+									<td>{item.nome}</td>
+									<td>{item.funcao}</td>
+									<td>{item.email}</td>
+								</tr>
+							))}
+						</tbody>
+					</StyledStaffTable>
+				) : (
+					<CardCorpoTecnico corpoTecnico={corpoTecnico} />
+				)}
 			</div>
 		</StyledStaffTableContainer>
 	);
