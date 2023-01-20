69c4e04e5f87bb0b8a4c5d2b27996259683bac09
Artur <arturrodriguesmota@hotmail.com>
2023-01-20 10:38:02 -0300

    :sparkles: feature(custom): ajeitando voltar ao topo mobile

--- src/components/basics/back-to-top/index.tsx
+++ src/components/basics/back-to-top/index.tsx
@@ -3,16 +3,32 @@ import { BackToTopContainer } from "./styles";
 
 function BackToTopIcon() {
 	return (
-		<svg
-			width="40"
-			height="40"
-			viewBox="0 0 40 40"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<circle cx="20" cy="20" r="20" fill="#9A1A4B" />
-			<path d="M10 27L20.3226 13L30 27" stroke="white" strokeWidth={5} />
-		</svg>
+		<section>
+		<div className="displayDesktop">
+			<svg
+				width="40"
+				height="40"
+				viewBox="0 0 40 40"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="20" cy="20" r="20" fill="#9A1A4B" />
+				<path d="M10 27L20.3226 13L30 27" stroke="white" strokeWidth={5} />
+			</svg>
+		</div>
+		<div className="displayMobile">
+			<svg
+				width="30"
+				height="30"
+				viewBox="0 0 45 45"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="20" cy="20" r="20" fill="#9A1A4B" />
+				<path d="M10 27L20.3226 13L30 27" stroke="white" strokeWidth={5} />
+			</svg>
+		</div>
+		</section>
 	);
 }
 
