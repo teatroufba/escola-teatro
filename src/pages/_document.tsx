d50c8feef132b64832ea23204d39a329a4896afb
Kelvin <kelvinsouza369@outlook.com>
2023-03-03 15:02:44 -0300

    :bug: fix(custom): remove layout shift

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,71 @@
-import BarraGoverno from "@/components/basics/BarraGoverno";
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
+import BarraGoverno from '@/components/basics/BarraGoverno'
+import Document, {
+    DocumentContext,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from 'next/document'
+import Script from 'next/script'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <BarraGoverno/>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            content="http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/10320"
-            property="creator.productor"
-          />
-          
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="True"/>
-          <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap" rel="stylesheet"/> 
-          <link rel="icon" type="image/x-icon" href="/brasao.png"/>
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
 
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <Script
-            src="http://barra.brasil.gov.br/barra.js"
-            type="text/javascript"
-          />
-        </body>
-      </Html>
-    );
-  }
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: [initialProps.styles, sheet.getStyleElement()],
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+        return (
+            <Html>
+                <BarraGoverno />
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta
+                        content="http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/10320"
+                        property="creator.productor"
+                    />
+
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="True"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link rel="icon" type="image/x-icon" href="/brasao.png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                    <Script
+                        src="http://barra.brasil.gov.br/barra.js"
+                        type="text/javascript"
+                    />
+                </body>
+            </Html>
+        )
+    }
 }
